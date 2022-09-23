6ea38f750956fe0cea59c8770441c05755e7ad62
upasnasingla <78492867+upasnasingla@users.noreply.github.com>
2022-09-24 00:02:13 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,17 @@
+//reverse a no
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+    int rev=0;
+    
+    while(n!=0){
+
+    int rem=n%10;
+    rev=rev*10+rem;
+    n=n/10;
+}
+    cout<<rev;
+	return 0;
+}
