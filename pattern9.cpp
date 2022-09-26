8a05b073c871d9a351456429591bbf17ad7d3f97
upasnasingla <78492867+upasnasingla@users.noreply.github.com>
2022-09-26 20:55:45 +0530

    Add files via upload

---
+++ pattern9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=n;i>=1;i--){
+		for(int space=1;space<n-i;space++){
+			cout<<" ";
+		}
+		for(int j=1;j<=i;j++){
+			cout<<"*";
+		}
+		cout<<endl;	
+		}
+	}
+
